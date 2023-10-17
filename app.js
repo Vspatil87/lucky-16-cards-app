f6da89b7630c8a6d54af55b9ef1d06215d9f0d3a
Vspatil <shirsathpatil87@gmail.com>
2023-10-17 07:15:17 +0530

    Initial UI setup

---
+++ app.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const url = require("url");
+const path = require("path");
+// const io = require('socket.io-client');
+// let socket = io.connect('http://localhost:8000/');
+let mainWindow;
+
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 1020,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            contextIsolation: true,
+        }
+    })
+
+    urlObject = {
+        pathname: path.join(__dirname, `app/index.html`),
+        protocol: "file:",
+        slashes: true
+    }
+    mainWindow.loadURL(
+        url.format(urlObject)
+    );
+
+    ipcMain.on('set-title', (event, title) => {
+        const webContents = event.sender
+        const win = BrowserWindow.fromWebContents(webContents)
+        win.setTitle(title);
+    })
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
